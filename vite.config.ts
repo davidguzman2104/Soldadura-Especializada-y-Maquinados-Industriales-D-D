5bd2992fcb75b9ecfa426f5010bba7870708d3ce
davidguzman2104 <l21200255@pachuca.tecnm.mx>
2026-01-18 22:02:05 -0600

    Fix base for pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/Soldadura-Especializada-y-Maquinados-Industriales-D-D/",
+ base: "/Soldadura-Especializada-y-Maquinados-Industriales-D-D/",
   server: {
     host: "::",
     port: 8080,
